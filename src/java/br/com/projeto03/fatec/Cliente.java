cd59225073dc9243c40936f2c661e43dfcefbda6
Raul <Raul@192.168.1.33>
2017-09-23 21:04:15 -0300

    Criação de classe Cliente - Raul

---
+++ src/java/br/com/projeto03/fatec/Cliente.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.projeto03.fatec;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Raul
+ */
+public class Cliente {
+    
+    private String nome;
+    private int cpf;
+    private int rg;
+    private String email;
+    private int telefone;
+    private String endereço;
+    
+    public static ArrayList<Cliente> cliente;
+    public static ArrayList<Cliente> getCliente()
+    {
+        if (cliente == null)
+        {
+            cliente = new ArrayList<>();
+        }
+        return cliente;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getRg() {
+        return rg;
+    }
+
+    public void setRg(int rg) {
+        this.rg = rg;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+    
+}
